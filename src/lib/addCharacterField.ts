7ecddc8f687e0bee0b0486680dd4459f8aaf9a63
megastruktur <astromortis@gmail.com>
2023-07-07 17:34:02 +0300

    Add the field no matter if it already exists.

--- src/lib/addCharacterField.ts
+++ src/lib/addCharacterField.ts
@@ -3,16 +3,7 @@ import type { Field } from "./types";
 
 export default function addCharacterField(character: CharactersResponse, field: Field) {
 
-  const foundIndex = character.fields.findIndex((f: Field) => f.name === field.name);
-
-  // Check if field exists. If so - notify.
-  if (foundIndex !== -1) {
-    console.log("Field already exists.")
-  }
-  // Create
-  else {
-    character.fields.push(field)
-  }
+  character.fields.push(field)
   return character
 
 }
