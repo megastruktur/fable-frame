c4ef7d813a5d876b4e6ed4f15146d335b7f3f741
megastruktur <astromortis@gmail.com>
2023-07-20 16:42:30 +0300

    Sidebar for mobile devices

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
 	import { currentUser } from "$lib/pocketbase";
 	import LogoutButton from "./LogoutButton.svelte";
+	import { drawerStore } from '@skeletonlabs/skeleton';
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
 </script>
 
-<nav class="list-nav">
+<nav class="list-nav p-4">
 	<ul>
-		<li><a href="/">Homepage</a></li>
+		<li><a on:click={drawerClose} href="/">Homepage</a></li>
 
 		{#if $currentUser}
-		<li><a href="/characters">Characters</a></li>
-		<li><a href="/campaigns">Campaigns</a></li>
-		<li><a class="btn variant-filled-success" href="/characters/create">Create Character</a></li>
+		<li><a on:click={drawerClose} href="/characters">Characters</a></li>
+		<li><a on:click={drawerClose} href="/campaigns">Campaigns</a></li>
+		<li><a on:click={drawerClose} class="btn variant-filled-success" href="/characters/create">Create Character</a></li>
 		<li><LogoutButton /></li>
 		{:else}
-			<li><a class="btn variant-filled-success" href="/login">Login</a></li>
+			<li><a on:click={drawerClose} class="btn variant-filled-success" href="/login">Login</a></li>
 		{/if}
 	</ul>
 </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang='ts'>
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { storePopup, type DrawerSettings } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
@@ -10,16 +10,36 @@
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	
+	const drawerSettings: DrawerSettings = {
+		id: 'navbar',
+		width: 'w-[170px]',
+	};
 </script>
 
+<Drawer>
+	<h2 class="h2 p-4">Navigation</h2>
+	<hr />
+	<Sidebar />
+</Drawer>
 <!-- App Shell -->
-<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-56">
 	<svelte:fragment slot="sidebarLeft">
     <Sidebar />
 	</svelte:fragment>
 	<svelte:fragment slot="header">
 		<AppBar>
 			<svelte:fragment slot="lead">
+				<button class="lg:hidden btn btn-sm mr-4" on:click={() => drawerStore.open(drawerSettings)}>
+					<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+					</span>
+				</button>
 				<strong class="text-xl uppercase">Fable Frame (WIP)</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
