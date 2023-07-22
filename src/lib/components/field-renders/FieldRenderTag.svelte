2940ddedfc48b1cd9fe3c3109483f0cf9bfdaa0b
megastruktur <astromortis@gmail.com>
2023-07-22 13:11:27 +0300

    Adds Field Remove feature

--- src/lib/components/field-renders/FieldRenderTag.svelte
+++ src/lib/components/field-renders/FieldRenderTag.svelte
@@ -4,16 +4,18 @@
 	import { createEventDispatcher } from "svelte";
 
   export let field: Field
+  export let classes: string = ""
+  export let editable: boolean = true
 
 
   const dispatch = createEventDispatcher()
 
 </script>
 
-<button class="btn" on:click>
-  {#if $editMode}
-    <input class="input" type="text" bind:value={field.value} on:focusout={() => {dispatch("fieldUpdate", field)}} />
+<div class="{classes} chip m-1" on:click on:keyup>
+  {#if editable && $editMode}
+    <input class="input" type="text" bind:value={field.label} on:focusout={() => {dispatch("fieldUpdate", field)}} />
   {:else}
-    <h4 class="h4">#{field.value ?? ""}</h4>
+    <h4 class="h4">{field.label ?? ""}</h4>
   {/if}
-</button>
+</div>
