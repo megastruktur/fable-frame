5ccf9b20682821f7923672acd354423885388103
megastruktur <astromortis@gmail.com>
2023-07-24 11:31:56 +0300

    Pocketbase URL as env variable

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,8 +1,8 @@
 import Pocketbase from "pocketbase";
 import { writable } from "svelte/store"
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 
-export const pb = new Pocketbase("http://127.0.0.1:8090");
-// export const pb = new Pocketbase("https://api.fable-frame.online");
+export const pb = new Pocketbase(PUBLIC_POCKETBASE_URL);
 
 export const currentUser = writable(pb.authStore.model)
 
