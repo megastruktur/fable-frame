be8fe72b86bf229fe55b1160b7c6b63000245998
megastruktur <astromortis@gmail.com>
2024-02-20 18:47:42 +0300

    Scene characters collapsed

--- src/lib/components/characters/CampaignCharactersDrawer.svelte
+++ src/lib/components/characters/CampaignCharactersDrawer.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { CampaignsResponse, CharactersResponse, RpgSystemsResponse } from "$lib/pocketbase-types";
+	import { Accordion, AccordionItem } from "@skeletonlabs/skeleton";
 	import CharacterSheet from "./CharacterSheet.svelte";
 
   export let characters: CharactersResponse[]
@@ -7,12 +8,23 @@
   export let campaign: CampaignsResponse
 </script>
 
-<div class="flex flex-col w-96">
-  {#each characters as character(character.id)}
-    <CharacterSheet
-      {character}
-      {rpgSystem}
-      {campaign}
-      compactVersion={true} />
-  {/each}
+<div class="flex flex-col w-96 p-3">
+
+  <Accordion
+      regionControl=""
+    >
+    {#each characters as character(character.id)}
+
+      <AccordionItem open={characters.length === 1}>
+        <svelte:fragment slot="summary">{character.name}</svelte:fragment>
+        <svelte:fragment slot="content">
+          <CharacterSheet
+          {character}
+          {rpgSystem}
+          {campaign}
+          compactVersion={true} />
+        </svelte:fragment>
+      </AccordionItem>
+    {/each}
+  </Accordion>
 </div>
